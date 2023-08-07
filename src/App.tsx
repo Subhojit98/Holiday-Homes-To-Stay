da6d0df45d5d98ea45f5a79b4cd6df3e3655c71d
Subhojit <subhojitchowhan4@gmail.com>
2023-08-08 03:06:12 +0530

    travel project stared

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-center w-1/2 h-1/2">dvsd</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Aleo': ['Aleo', 'serif'],
+        'IBM': ['IBM Plex Mono', 'serif'],
+        'chakra': ['Chakra Petch'],
+        'Manrope': ['Manrope'],
+        'Josefin': ['Josefin'],
+        'Lato': ['Lato'],
+        'Nunito': ['Nunito'],
+        'Poppins': ['Poppins'],
+        'Quicksand': ['Quicksand'],
+        'Raleway': ['Raleway'],
+        'Roboto': ['Roboto'],
+        'RobotoMono': ['Roboto Mono'],
+        'Rubik': ['Rubik'],
+        'SpaceMono': ['Space Mono'],
+        'TiltNeon': ['Tilt Neon'],
+        'Ubuntu': ['Ubuntu'],
+        'VarelaRound': ['Varela Round'],
+        'ZillaSlab': ['Zilla Slab'],
+
+
+      },
+      backgroundColor: {
+        WholePage: '#f0ece9',
+        specialBg: '#c6b5aa',
+        buttonsBg: '#222222'
+      },
+      textColor: {
+        defaultTextColor: '#81807f',
+        defaultTextColor2: '#7a7977',
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
