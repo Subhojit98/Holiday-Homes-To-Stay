2d351f5cfdad33cd5ec4d71ece0f5fe5827c75c7
Subhojit <subhojitchowhan4@gmail.com>
2024-04-03 12:51:06 +0530

    Update homepage and base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Holiday_Homes_To_Stay/",
+  base: "/Holiday-Homes-To-Stay/",
   plugins: [react()],
 })
