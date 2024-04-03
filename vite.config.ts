89dabc1d578cf62fbcc64c21ae92c74b21a3e77f
Subhojit <subhojitchowhan4@gmail.com>
2024-04-03 12:14:30 +0530

    some updates in vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Holiday_Homes_To_Stay/",
   plugins: [react()],
-  base: '/Holiday_Homes_To_Stay/'
 })
