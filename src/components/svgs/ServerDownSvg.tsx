dd930c2bf5220ee2f89cd821a56e0c356a359f07
Subhojit <subhojitchowhan4@gmail.com>
2024-04-05 00:28:07 +0530

    Fix broken image source in ServerDownSvg component

--- src/components/svgs/ServerDownSvg.tsx
+++ src/components/svgs/ServerDownSvg.tsx
@@ -1,9 +1,9 @@
-import errorImg from '/src/assets/logo/server down.svg'
+// import errorImg from '/src/assets/logo/server down.svg'
 
 const ServerDownSvg = () => {
     return (
         <>
-            <img src={errorImg} alt="Page not found" className="object-cover" />
+            <img src={""} alt="Page not found" className="object-cover" />
         </>
     )
 }
